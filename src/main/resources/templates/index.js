51c0198c8c2d4b5c4e849a365e2f436c2902169f
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-10-08 18:09:32 +0300

    Fetch data from jsonplaceholder

--- src/main/resources/templates/index.js
+++ src/main/resources/templates/index.js
@@ -1,7 +1,26 @@
 "use strict";
 
-const ReactAppFromCDN = () => {
-  return <div>My React App with CDN</div>;
-};
+class PersonList extends React.Component {
+  state = {
+    persons: [],
+  };
 
-ReactDOM.render(<ReactAppFromCDN />, document.querySelector("#root"));
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+      const persons = res.data;
+      this.setState({ persons });
+    });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.persons.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+ReactDOM.render(<PersonList />, document.querySelector("#root"));
