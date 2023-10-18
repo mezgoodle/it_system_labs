f4c497793758ea9709b416ae14db2b4dbc7e7418
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-10-18 09:27:22 +0300

    Install axios

--- open_data/src/App.jsx
+++ open_data/src/App.jsx
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <>
       <div className="h1">Hello</div>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
